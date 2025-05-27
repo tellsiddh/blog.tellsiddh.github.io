3031f223cec146cf0feb6af72f9eb7657beaeb39
Siddharth Jain <tellsiddh@gmail.com>
2025-05-26 19:43:32 -0700

    fmt

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "My First Post",
+        "excerpt":"This is my first blog post using Jekyll and Minimal Mistakes!   Stay tuned for more content 🚀  ","categories": ["personal"],
+        "tags": ["jekyll","blogging","first-post"],
+        "url": "/personal/2025/05/26/first-post.html",
+        "teaser": null
+      }]
